ce68875d1eb94973777ed9d4f0a3572bd7d3f933
polymagnus <matt_barbour@cable.comcast.com>
2014-11-20 20:23:18 -0700

    more tweeks

--- javascripts/names.js
+++ javascripts/names.js
@@ -1,11 +1,16 @@
 
-    var nounArray = [	'Rahul', 'Magnus', 'Vicente', 'Amy', 'Kevin', 'Sean', 'Hoa', 'Justin','Jonathan','Josh', '#hayashi',
+    var nounArray = [	'Rahul', 'Matt', 'Vicente', 'Amy', 'Kevin', 'Sean', 'Hoa', 'Justin','Jonathan','Josh', 'Tad',
+    					'Chhhhhhhhhhabria', 'Magnus', 'Hoalton',  
     					'Pony', 'Sheep', 'Horse', 'Cow', 'Bear', 'Otter', 'Man', 'Boy',
     					'Dildo', 'Douche', 'Dick', 'Cock',
     					'Fairy', 'Princess', 'Queen'];
     var adjArray = [	'Reamers', 'Slammers', 'Lickers', 'Pleasers', 'Whores', 'Lovers',
-    					'Slappers', 'Caressers', 'Molesters', 'Offenders', 'Power-Bottoms',
-    					'Cuddlers'];
+    					'Slappers', 'Caressers', 'Molesters', 'Sex Offenders', 'Power Bottoms',
+    					'Cuddlers', 'Gaywads', 'Horse Cocks', 'Limp Dicks'];
 
-    var hoaRray = ['wa', 'wha' ];
+    var hoaRray = ['wa', 'wha', 'ho'];
     var nguyenRay = ['when', 'win']
+
+
+
+
--- javascripts/shameController.js
+++ javascripts/shameController.js
@@ -6,30 +6,58 @@
     
     module.controller('shameDispenser', function($scope) {
 
-        $scope.shamename='Fairy Princesses';
+        $scope.shamename=generateNewRandomName();
 
         $scope.generateName = function(){
-
-            var randomNoun=Math.floor(nounArray.length*Math.random());
-            var randomAdj=Math.floor(adjArray.length*Math.random());
-
-            $scope.shamename =  nounArray[randomNoun].toLowerCase() + " " + 
-                                adjArray[randomAdj].toLowerCase();
+            $scope.shamename = generateNewRandomName();
         }
 
         $scope.hoaify = function(){
             var tempName = $scope.shamename
 
-            // Change out all the first name
+            // Change out all the first name possibilities
             for (var replaceMe in hoaRray){
-                tempName = tempName.toLowerCase().replace(hoaRray[replaceMe], 'Hoa')
+                tempName = tempName.toLowerCase().replace(hoaRray[replaceMe], 'hoa')
             }
 
-            // Change our all the last name
+            // other odd cases
+            tempName = tempName.toLowerCase().replace(' o', ' hoa')
+            
+            // Change our all the last name possibilities
             for (var replaceMe in nguyenRay){
                 tempName = tempName.toLowerCase().replace(nguyenRay[replaceMe], 'Nguyen')
             }            
             $scope.shamename = tempName;
+        }
+
+        $scope.tadify = function(){
+            var tempName = $scope.shamename
+            tempName = tempName.replace('#', '');
+            tempName = "#"+tempName.replace(' ', '');
+
+            tempName = tempName.toLowerCase().replace('you', 'u')
+            tempName = tempName.toLowerCase().replace('are', 'r')
+                        
+            $scope.shamename = tempName;
+        }
+
+        $scope.chhabrify = function(){
+            var tempName = $scope.shamename
+
+            tempNameArr = tempName.toLowerCase().split(' ');
+            tempName = tempNameArr[0] + "'s gay";
+                        
+            $scope.shamename = tempName;
+        }
+
+        function generateNewRandomName() {
+            var randomNoun=Math.floor(nounArray.length*Math.random());
+            var randomAdj=Math.floor(adjArray.length*Math.random());
+            console.log(randomNoun);
+            console.log(randomAdj);
+
+            return nounArray[randomNoun].toLowerCase() + " " + 
+                                adjArray[randomAdj].toLowerCase();   
         }
 
     });
